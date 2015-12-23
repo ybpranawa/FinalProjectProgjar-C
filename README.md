a29bddad85aefd4b98f7a0331fcd2558d257f05d
Fendy <fendy.fendy95@gmail.com>
2015-12-23 15:21:50 +0700

    Client forms created

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# FinalProjectProgjar-C
+# FinalProjectProgjar-C  
 
-Kelompok
-Fendy Purnama
-Yoga Bayu Aji Pranawa
-Yuna Sugianella
-Ignatius Bennedict
+Kelompok  
+Fendy  
+Yoga Bayu Aji Pranawa  
+Yuna Sugianella  
+Ignatius Benedict  
---
+++ src/adupintar/client/AduPintar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package adupintar.client;
+
+/**
+ *
+ * @author fendy
+ */
+public class AduPintar {
+    public static void main(String args[]) {
+        Login loginForm = new Login(null);
+        loginForm.showForm();
+    }
+}
