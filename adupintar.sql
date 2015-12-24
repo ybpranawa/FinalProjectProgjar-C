72c2c8c7f6a6f10912b8279ddd515cf13cc80567
Yoga Bayu Aji Pranawa <khususlain@yahoo.com>
2015-12-25 01:22:02 +0700

    exported database for adupintar

---
+++ adupintar.sql
@@ -0,0 +1,33 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.6.21 : Database - adupintar
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `user` */
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `username` varchar(100) NOT NULL DEFAULT '',
+  `name` varchar(100) DEFAULT '',
+  `password` varchar(100) DEFAULT '',
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `user` */
+
+insert  into `user`(`username`,`name`,`password`) values ('asd','asd','qwe'),('qwe','qwe','qwe'),('zxc','zxc','zxc');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
