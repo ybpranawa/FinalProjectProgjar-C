1b990d1d9f847abf480421a6c980f9969ffc5efd
Fendy <fendy.fendy95@gmail.com>
2016-01-01 16:08:55 +0700

    Updated README and db structure

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
-# FinalProjectProgjar-C (client)
+# FinalProjectProgjar-C
 
-Kelompok  
+Kelompok  Adu Pintar
 Fendy  
 Yoga Bayu Aji Pranawa  
 Yuna Sugianella  
 Ignatius Benedict  
+
+# Project List
+1. Server -- Adu Pintar server  
+2. Client -- Adu Pintar client  
+3. Object Serialization -- Adu Pintar object used for communication  
+All of them are netbeans projects
--- server/adupintar.sql
+++ server/adupintar.sql
@@ -1,33 +1,64 @@
-/*
-SQLyog Ultimate v11.11 (64 bit)
-MySQL - 5.6.21 : Database - adupintar
-*********************************************************************
-*/
+-- phpMyAdmin SQL Dump
+-- version 4.3.11
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 01, 2016 at 10:01 AM
+-- Server version: 5.6.24
+-- PHP Version: 5.6.8
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
-/*!40101 SET SQL_MODE=''*/;
+--
+-- Database: `adupintar`
+--
 
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-/*Table structure for table `user` */
+-- --------------------------------------------------------
 
-DROP TABLE IF EXISTS `user`;
+--
+-- Table structure for table `friendships`
+--
 
-CREATE TABLE `user` (
-  `username` varchar(100) NOT NULL DEFAULT '',
-  `name` varchar(100) DEFAULT '',
-  `password` varchar(100) DEFAULT '',
-  PRIMARY KEY (`username`)
+CREATE TABLE IF NOT EXISTS `friendships` (
+  `me` varchar(100) NOT NULL,
+  `friend` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `user` */
+-- --------------------------------------------------------
 
-insert  into `user`(`username`,`name`,`password`) values ('asd','asd','qwe'),('qwe','qwe','qwe'),('zxc','zxc','zxc');
+--
+-- Table structure for table `users`
+--
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE IF NOT EXISTS `users` (
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `friendships`
+--
+ALTER TABLE `friendships`
+  ADD PRIMARY KEY (`me`,`friend`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
